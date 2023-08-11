75397c4011757200362b0316fe9ee254f22c3ea2
Shannqa <shannqa@gmail.com>
2023-08-11 22:42:07 +0200

    Test test

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//test 1 2
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLFUiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kcm9wZG93bi1tZW51Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vdGVzdCAxIDIiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
