2514e7c5fb2d1542edd6af2674309cf48c8905ee
Shannqa <shannqa@gmail.com>
2023-08-15 12:42:50 +0200

    Add absolute positioning and some small fixes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@
   
   function createMenu() {
     const menuItems = [
-        { ID: "1", label: "One", url: "http://google.com" },
-        { ID: "2", label: "Two", url: "http://youtube.com" },
+        { ID: "1", label: "One", url: "http://example.com" },
+        { ID: "2", type: "group", label: "Two", url: "http://example.com", options: [
+          { label: "Two-one", url: "http://example.com" },
+          { label: "Two-two", url: "http://example.com"},
+      ]},
         { ID: "3", type: "group", label: "Three", options: [
-            { label: "Three-one", url: "http://wikipedia.com" },
+            { label: "Three-one", url: "http://example.com" },
             { label: "Three-two", url: "http://example.com"},
         ]},
         { ID: "4", type: "group", label: "Four", options: [
-            { label: "Four-one", url: "#"},
-            { label: "Four-two", url: "#" },
-            { label: "Four-three", url: "#"},
+            { label: "Four-one", url: "http://example.com"},
+            { label: "Four-two", url: "http://example.com" },
+            { label: "Four-three", url: "http://example.com"},
         ]}
         ];
 
