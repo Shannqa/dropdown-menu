3c3c15b89416de8360b4717a1ed375a2112ffb6c
Shannqa <shannqa@gmail.com>
2023-08-28 19:14:54 +0200

    Add media queries

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
   import css from './styles.css';
+  import menuHamb from './menu_hamb.svg'
+
   
   function createMenu() {
     const menuItems = [
         { ID: "1", label: "One", url: "http://example.com" },
-        { ID: "2", type: "group", label: "Two", url: "http://example.com", options: [
+        { ID: "2", type: "group", label: "Two", options: [
           { label: "Two-one", url: "http://example.com" },
           { label: "Two-two", url: "http://example.com"},
       ]},
@@ -15,15 +17,27 @@
             { label: "Four-one", url: "http://example.com"},
             { label: "Four-two", url: "http://example.com" },
             { label: "Four-three", url: "http://example.com"},
-        ]}
+        ]},
+        { ID: "4", label: "Five", url: "http://example.com" },
+        { ID: "5", label: "Six", url: "http://example.com" },
+        { ID: "6", label: "Seven", url: "http://example.com" },
+        { ID: "7", label: "Eight", url: "http://example.com" },
+        { ID: "8", label: "Nine", url: "http://example.com" },
         ];
 
     const body = document.querySelector("body");
     const container = document.createElement("div");
     container.classList.add("menu-container");
+    const menuHamburger = document.createElement("div");
+    const menuIcon = new Image();
+    menuIcon.src = menuHamb;
+    menuHamburger.appendChild(menuIcon);
+    menuHamburger.classList.add("menu-hamburger");
+    container.appendChild(menuHamburger);
   
     menuItems.forEach((item) => {
       const columnDiv = document.createElement("div");
+      columnDiv.classList.add("menu-column");
       container.appendChild(columnDiv);
       const label = document.createElement("a");
       label.classList.add("menu-label");
@@ -33,7 +47,7 @@
       }
       columnDiv.appendChild(label);
             
-      if (item.type === "group") {
+      if (item.type === "group") { 
         label.addEventListener("mouseover", () => {
           let IDoptions = document.querySelector(`.menu-id-${item.ID}`);
           IDoptions.classList.add("menu-visible");
@@ -41,6 +55,11 @@
         columnDiv.addEventListener("mouseleave", () => {
         let IDoptions = document.querySelector(`.menu-id-${item.ID}`);
         IDoptions.classList.remove("menu-visible");
+      });
+
+      label.addEventListener("click", () => {
+        let IDoptions = document.querySelector(`.menu-id-${item.ID}`);
+        IDoptions.classList.toggle("mobile-visible");
       });
         const optionsDiv = document.createElement("div");
         optionsDiv.classList.add("menu-options", `menu-id-${item.ID}`);
@@ -57,6 +76,18 @@
       }
     });
     body.appendChild(container);
+    menuHamburger.addEventListener("click", () => {
+      const columns = document.getElementsByClassName("menu-column");
+      // columns.classList.add("visible");
+      for (let i = 0; i < columns.length; i++) {
+        const element = columns[i];
+        element.classList.toggle("visible");
+        
+      }
+      console.log(columns);
+    });
   }
   
   createMenu();
+
+  
